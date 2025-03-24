d1de230c6cd12eae9cded0fa2a69accf3b6800f3
sachin.malvi <sachin.malvi@hcl.com>
2025-03-24 21:46:00 +0530

    Code commit

---
+++ core-customize/hybris/bin/custom/myproject/myprojectbackoffice/gensrc/org/myprojectbackoffice/constants/GeneratedYacceleratorbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Dec 5, 2024, 4:58:58 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package org.myprojectbackoffice.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYacceleratorbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "myprojectbackoffice";
+	
+	protected GeneratedYacceleratorbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ core-customize/hybris/bin/custom/myproject/myprojectinitialdata/gensrc/de/hybris/myproject/initialdata/constants/GeneratedMyprojectInitialDataConstants.java
@@ -0,0 +1,26 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Dec 5, 2024, 4:58:58 PM                     ---
+ * ----------------------------------------------------------------
+ *  
+ * Copyright (c) 2024 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package de.hybris.myproject.initialdata.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedMyprojectInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "myprojectinitialdata";
+	
+	protected GeneratedMyprojectInitialDataConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
