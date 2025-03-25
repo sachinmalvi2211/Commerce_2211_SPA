6751d79ce82fc619ab4e1b924365b161792506db
sachin.malvi <sachin.malvi@hcl.com>
2025-03-25 18:34:45 +0530

    removed smartedittrail custom extension

--- core-customize/hybris/bin/custom/smartedittrail/apps/smartedittrailcommons/README.md
+++
@@ -1,1 +0,0 @@
-## smartedittrail Commons
--- core-customize/hybris/bin/custom/smartedittrail/apps/smartedittrailcommons/src/services/index.ts
+++
@@ -1,5 +0,0 @@
-/*
- * Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved.
- */
-export * from './AbAnalyticsModule';
-export * from './AbAnalyticsService';
